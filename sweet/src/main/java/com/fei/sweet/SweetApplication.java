97e8083a37e7652ef3bdf07d6f3d11c0d6153ab4
fei <feiainan150623@163.com>
2017-12-26 17:47:57 +0800

    第一次

---
+++ sweet/src/main/java/com/fei/sweet/SweetApplication.java
@@ -0,0 +1,15 @@
+package com.fei.sweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class SweetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetApplication.class, args);
+	}
+}
